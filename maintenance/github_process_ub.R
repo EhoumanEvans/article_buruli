3822dee864c38219b513501e4996d96f457fea8f
EhoumanEvans <ehoumanevans@gmail.com>
2021-11-22 00:49:58 +0000

    add github_process to maintenance folder

---
+++ maintenance/github_process_ub.R
@@ -0,0 +1,41 @@
+# Check your configuration
+usethis::git_sitrep()
+git_vaccinate()
+# Set your configuration
+usethis::use_git_config(
+  user.name = "EhoumanEvans",
+  user.email = "ehoumanevans@gmail.com")
+
+
+# Configure Git after installation
+library(usethis)
+edit_git_config()
+EhoumanEvans
+
+# Create a Personal Access Token (PAT) on GitHub
+library(usethis)
+
+create_github_token()
+  # Copy the token Generated in Github
+  # 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
+
+# Relaunch RStudio
+# Store Personal Access Token to Connect RStudio and GitHub
+
+library(gitcreds)
+gitcreds_set()
+
+
+  # Relaunch RStudio
+# Connect RStudio Projects with GitHub Repositories
+# RStudio First
+library(usethis)
+use_github()
+
+# ensuite 
+
+# git remote add origin https://github.com/EhoumanEvans/article_buruli.git
+git branch -M main
+git push -u origin main
+
+
